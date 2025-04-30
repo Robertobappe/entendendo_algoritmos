deaa13256686b61b0c52bf1eb654b997c29b275e
Roberto Silva <robertobs@usp.br>
2025-04-29 23:31:38 -0300

    vf

--- pesquisa_binaria.py
+++ pesquisa_binaria.py
@@ -1,3 +1,8 @@
+#Implementação do algoritmo da pesquisa binária
+#Sua entrada é uma lista ordenada
+#Se o elemento que vc esta procurando esta na lista, retornará sua localização
+#Caso contrário, None
+
 def pesquisa_binaria(lista, item):
     baixo = 0
     alto = len(lista) - 1
@@ -8,8 +13,10 @@ def pesquisa_binaria(lista, item):
 
         if chute == item:
             return meio
+        #chute foi mt alto
         if chute > item:
             alto = meio - 1
+        #chute foi mt baixo
         else:
             baixo = meio + 1
     return None
@@ -18,3 +25,6 @@ minha_lista = [1,3,5,7,9]
 
 print(pesquisa_binaria(minha_lista, 3))
 print(pesquisa_binaria(minha_lista, -1))
+
+#Se tivermos uma lista com 128 nomes --> log128
+# x = 8 será o número máximo de etapas para encontrar o item na lista
